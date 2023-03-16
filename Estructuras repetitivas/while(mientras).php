d9ff00caff70dfca12d7fd30719a12d9797c39af
Matias <matiascascini87@gmail,com>
2023-03-16 16:07:00 -0300

    agregue la estructura repetitiva while

---
+++ Estructuras repetitivas/while(mientras).php
@@ -0,0 +1,15 @@
+<?php
+
+/*$contador = 10;
+while($contador > 10){
+    echo  $contador;
+    $contador--;
+}
+*/
+
+echo "la puta  madre, ¿por que no anda esto?".PHP_EOL;
+$a= 23;
+$b= 78;
+echo $a + $b;
+
+?>
