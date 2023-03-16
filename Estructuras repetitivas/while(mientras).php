b2b5ff89bdd8ee767d7c65907008f382c6d26f6b
Matias <matiascascini87@gmail,com>
2023-03-16 17:13:52 -0300

    agregue ejemplos de while y do while

--- Estructuras repetitivas/while(mientras).php
+++ Estructuras repetitivas/while(mientras).php
@@ -1,15 +1,28 @@
 <?php
 
-/*$contador = 10;
-while($contador > 10){
-    echo  $contador;
+$contador = 10;
+while($contador <= 10){
+    echo  $contador.PHP_EOL;
     $contador--;
+    if ($contador == 5){
+        echo "El contador llegó a 5".PHP_EOL;
+        break;
+    }
 }
-*/
 
-echo "la puta  madre, ¿por que no anda esto?".PHP_EOL;
-$a= 23;
-$b= 78;
-echo $a + $b;
+echo "---DO WHILE---".PHP_EOL;
+
+$contador=10;
+do {
+    echo "cuenta regresiva: $contador".PHP_EOL;
+    $contador --;
+} while ($contador > 10);
+
+/* el do while lo que hace es entrar al bucle sin verificar la condicion del while
+y luego evalua, por ejemplo, en el anterior, entra porque $contador = 10 y nos devuelve
+"cuenta regresiva: 10", pero cuando llega al while y evalua nos da False, por lo que corta
+el bucle.
+*/ 
+
 
 ?>
